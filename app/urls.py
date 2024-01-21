77cc73712fa0482261d85b559f419877be4ae4d6
Rodrigo Medeiros <rodrigovinicius.bsb@gmail.com>
2024-01-21 12:07:05 -0300

    django template start

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from app.views import hello, job_detail
+# from app.views import hello, job_detail
+from app import views 
+
 
 urlpatterns = [
-    path('', hello),
-    path('job/<int:id>', job_detail)
+    path('', views.job_list, name='jobs_home'),
+    path('hello/', views.hello, name='hello'),
+    path('job/<int:id>', views.job_detail, name='jobs_detail'),
+    # path('job/<str:id>', views.job_detail),
+    # path('job/1', views.job_detail),
 ]
--- app/views.py
+++ app/views.py
@@ -1,30 +1,49 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import redirect 
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.template import loader
 
 job_title = [
     "First Job",
-    "Second Job"
+    "Second Job",
+    "Third Job"
 ]
 
 job_description = [
-    "First Job description",
-    "Second Job description",
+    "First job description",
+    "Second job description",
+    "Third job description"
 ]
 
-# Create your views here.
+class TempClass:
+    x = 5
+
 def hello(request):
-    list_job = []
-    for count in range(2):
-        str = f"<ul><li>Job {count}: Title: {job_title[count]}, Description: {job_description[count]}</ul></li>"
-        list_job.append(str)
-        
-    return HttpResponse(list_job)
+    list = ["alpha", "beta"]
+    temp = TempClass()
+    is_authenticated = False
+    context={"name":"Django", "age":30, "first_list":list, "temp_object":temp,
+            "is_authenticated":is_authenticated}
+    return render(request, "app/hello.html",context)
+
+def job_list(request):
+    list_of_jobs = "<ul>"
+    for j in job_title:
+        job_id = job_title.index(j)
+        detail_url = reverse('jobs_detail', args=(job_id,))
+        list_of_jobs += f"<li><a href='{detail_url}'>{j}</a></li>"
+    list_of_jobs += "</ul>"
+    return HttpResponse(list_of_jobs)
+
 
 def job_detail(request, id):
     print(type(id))
-    
-    if id == 0:
-        return redirect('/')
-    return_html = f"<h1>{job_title[id]}</h1> <h3>{job_description[id]}</h3>"
-    return HttpResponse(return_html)
+
+    try:
+        if id == 0:
+            return redirect(reverse('jobs_home'))
+        return_html = f"<h1>{job_title[id]}</h1> <h3>{job_description[id]}</h3>"
+        return HttpResponse(return_html)
+    except:
+        return HttpResponseNotFound("Not Found")
