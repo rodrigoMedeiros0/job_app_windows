8492e565549bd9332d6a5376aa92c3deb741a78c
Rodrigo Medeiros <rodrigovinicius.bsb@gmail.com>
2024-01-27 22:01:10 -0300

    database sql + model

--- app/urls.py
+++ app/urls.py
@@ -6,8 +6,6 @@ from app import views
 
 urlpatterns = [
     path('', views.job_list, name='jobs_home'),
-    path('hello/', views.hello, name='hello'),
     path('job/<int:id>', views.job_detail, name='jobs_detail'),
-    # path('job/<str:id>', views.job_detail),
-    # path('job/1', views.job_detail),
+      path('hello/', views.hello, name='hello'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,47 +3,47 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.template import loader
+from app.models import JobPost
 
 job_title = [
     "First Job",
     "Second Job",
-    "Third Job"
+    "Third  Job"
 ]
 
 job_description = [
-    "First job description",
-    "Second job description",
-    "Third job description"
+    "First Job description",
+    "Second Job description",
+    'Third Job Description'
 ]
+
+# Create your views here.
 
 class TempClass:
     x = 5
 
-def hello(request):
-    list = ["alpha", "beta"]
-    temp = TempClass()
-    is_authenticated = False
-    context={"name":"Django", "age":30, "first_list":list, "temp_object":temp,
-            "is_authenticated":is_authenticated}
-    return render(request, "app/hello.html",context)
-
 def job_list(request):
-    list_of_jobs = "<ul>"
-    for j in job_title:
-        job_id = job_title.index(j)
-        detail_url = reverse('jobs_detail', args=(job_id,))
-        list_of_jobs += f"<li><a href='{detail_url}'>{j}</a></li>"
-    list_of_jobs += "</ul>"
-    return HttpResponse(list_of_jobs)
+    jobs = JobPost.objects.all()
+    context={"jobs": jobs} 
+    return render(request, "app/index.html", context)
 
 
 def job_detail(request, id):
-    print(type(id))
 
     try:
-        if id == 0:
+        if id == 1:
             return redirect(reverse('jobs_home'))
-        return_html = f"<h1>{job_title[id]}</h1> <h3>{job_description[id]}</h3>"
-        return HttpResponse(return_html)
+        job = JobPost.objects.get(id=id)
+        context={"job": job }
+        return render(request, "app/job_detail.html", context)
     except:
-        return HttpResponseNotFound("Not Found")
+        return HttpResponseNotFound("Page not found.")
+    
+def hello(request):
+    list = ["Elefante", "Tigre"]
+    temp = TempClass()
+    is_authenticated = False
+    context={"name": "Django", "first_list": list, "temp_object": temp, "age": 29,
+             "is_authenticated": is_authenticated}
+    return render(request, "app/hello.html", context)
+    
